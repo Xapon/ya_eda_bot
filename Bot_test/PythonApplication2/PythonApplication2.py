95a1584c6a9507558da323ef5ce65a9783aa10e3
Xapon <79209665+Xapon@users.noreply.github.com>
2023-08-02 16:01:30 +0300

    Delete Bot_test directory

--- Bot_test/PythonApplication2/PythonApplication2.py
+++
@@ -1,8 +0,0 @@
-import telebot
-bot = telebot.TeleBot('TOKEN')
-
-@bot.message_handler(commands=['start'])
-def copy_past(message):
-    s1 = message
-    bot.reply_to(message.from_user.id, s1)
-bot.polling(none_stop=True)
